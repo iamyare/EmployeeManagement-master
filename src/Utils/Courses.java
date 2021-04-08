a1c3cffadb87c066b75511c1d6e621743b93957e
iamyare <yarephotographer@gmail.com>
2021-04-07 23:03:53 -0600

    Version 1

---
+++ src/Utils/Courses.java
@@ -0,0 +1,14 @@
+package Utils;
+
+public class Courses {
+    public static String Nombre_Empresa = "Domestika";//Empresa que ofrece los cursos
+    public static double price = 12.2;
+
+
+    public static void Cursos(){
+        System.out.println("Programacion en Java");
+        System.out.println("Programacion en C++");
+        System.out.println("Introduccion a Inteligencia Artificial");
+        System.out.println("Marketing");
+    }
+}
